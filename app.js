e804577376c9daf6f97afd246a3f83a3eab003bc
Zack Hanni <zack.hannI@gmail.com>
2023-07-05 16:44:03 -0400

    working but zooms in

---
+++ app.js
@@ -0,0 +1,30 @@
+(function () {
+    const pictures = [
+        "img1","img2","img3"
+    ];
+
+    const buttons = document.querySelectorAll(".btn");
+    const imgDiv = document.querySelector(".img-container");
+
+    let counter = 0;
+
+    buttons.forEach(function (button) {
+        button.addEventListener("click", function (e) {
+              if (button.classList.contains('btn-left')){
+                counter--
+                if(counter < 0){
+                    counter = pictures.length -1
+                }
+                imgDiv.style.background = `url("img/${pictures[counter]}.jpg")`
+              }
+
+              if (button.classList.contains('btn-right')){
+                counter++
+                if(counter > pictures.length -1){
+                    counter = 0
+                }
+                imgDiv.style.background = `url("img/${pictures[counter]}.jpg")`
+              }
+        })
+    });
+})()
